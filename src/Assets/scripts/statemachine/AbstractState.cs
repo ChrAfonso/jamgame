53a6c73662cdb90b6e19f20113c7e1bd0df91ea9
rangermann <rainer@rarebyte.com>
2014-09-26 20:53:51 +0200

    created Unity project

---
+++ src/Assets/scripts/statemachine/AbstractState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public abstract	class AbstractState {
+
+  public string StateName { get; private set; }
+
+  public AbstractState(string stateName) {
+    StateName = stateName;
+
+    OnInitialize();
+  }
+
+  public abstract void OnInitialize();
+  public abstract void OnEnter(object onEnterParams = null);
+  public abstract void OnLeave();
+  public abstract void OnUpdate();
+  public virtual void OnGUI() { }
+}
