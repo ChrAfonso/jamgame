3df750698e371b9403c06df3e3714f3700bc44fa
patience1312 <horstschnattler@cp20-92.tu-graz.ac.at>
2014-09-27 18:09:10 +0200

    Die Sound und Music Manager Basics

---
+++ src/Assets/musicManagerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class musicManagerScript : MonoBehaviour {
+
+	public AudioClip[] songs;
+
+	// Use this for initialization
+	void Start () {
+		audio.clip = songs[0];
+		audio.Play ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
