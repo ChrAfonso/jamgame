22f2b6f1a442e11b2fb6e6a985a50bbcbfaa15af
Christian Afonso <chr.afonso@gmail.com>
2014-09-27 11:36:37 +0200

    Player moves with default speed, rotates with l/r keys

--- src/Assets/scripts/game/player/PlayerController.cs
+++ src/Assets/scripts/game/player/PlayerController.cs
@@ -2,29 +2,32 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
-  public float DDirection { get; private set; }
-  public GameObject go { get; set; }
   
-	// Use this for initialization
-	public void Start () {
-	  
-	}
-	
-	// Update is called once per frame
-	public void Update () {
+  public float Speed { get; set; } // TODO: set default speed, if sped up/slowed down by jam, ease back into default speed slowly?
+  public Vector3 Direction { get; set; }
+  private float DDirection;
+  public float DirectionChangeSpeed { get; set; }
+  
+  // Use this for initialization
+  public void Start () {
+    Speed = 5;
+    Direction = new Vector3(1, 0, 0);
+    DirectionChangeSpeed = 200;
+  }
+  
+  // Update is called once per frame
+  public void Update () {
     if(Input.GetKeyDown(KeyCode.LeftArrow)) {
       DDirection = -1;
     } else if(Input.GetKeyDown(KeyCode.RightArrow)) {
       DDirection = 1;
-    }
-
-    if(Input.GetKeyUp(KeyCode.LeftArrow)) {
+    } else if(Input.GetKeyUp(KeyCode.LeftArrow) && DDirection == -1) {
       DDirection = 0;
-    } else if(Input.GetKeyUp(KeyCode.RightArrow)) {
+    } else if (Input.GetKeyUp(KeyCode.RightArrow) && DDirection == 1) {
       DDirection = 0;
     }
 
-    transform.position = new Vector3(transform.position.x + DDirection * Time.deltaTime, transform.position.y, transform.position.z);
-	}
+    Direction = Quaternion.AngleAxis(DDirection * DirectionChangeSpeed * Time.deltaTime, new Vector3(0, 0, 1)) * Direction;
+    transform.position += (Direction * (Speed * Time.deltaTime));
+  }
 }
