3833c12da5b91c711e742151defa70d8cbe67881
rangermann <rainer@rarebyte.com>
2014-09-28 12:42:10 +0200

    game start/title UI

--- src/Assets/scripts/game/states/GameStateCredits.cs
+++ src/Assets/scripts/game/states/GameStateCredits.cs
@@ -2,28 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
-  public class GameStateCredits : AbstractState  {
+public class GameStateCredits : AbstractState {
 
-    public GameStateCredits(string stateName)
-      : base(stateName) {
-    }
+  public GameStateCredits(string stateName)
+    : base(stateName) {
+  }
 
-    public override void OnInitialize() {
-     
-    }
+  public override void OnInitialize() {
 
-    public override void OnEnter(object onEnterParams = null) {
+  }
 
-		GameController.Instance.MusicManager.PlayTrack(MusicManager.Theme.Credits);
-      
-    }
+  public override void OnEnter(object onEnterParams = null) {
+    GameController.Instance.MusicManager.PlayTrack(MusicManager.Theme.Credits);
+  }
 
-    public override void OnLeave() {
-      
-    }
+  public override void OnLeave() {
 
-    public override void OnUpdate() {
-      
+  }
+
+  public override void OnUpdate() {
+    if (Input.GetKeyUp(KeyCode.Escape)) {
+      GameController.Instance.ChangeState("GameStateIntro");
     }
   }
+}
--- src/Assets/scripts/game/states/GameStateIntro.cs
+++ src/Assets/scripts/game/states/GameStateIntro.cs
@@ -6,26 +6,71 @@ using UnityEngine;
 
   public class GameStateIntro : AbstractState  {
 
+    private GameObject goTitle { get; set; }
+
+    private Rect rectButtonStart { get; set; }
+
+    private Rect rectButtonCredits { get; set; }
+
     public GameStateIntro(string stateName)
       : base(stateName) {
     }
 
     public override void OnInitialize() {
-     
+      goTitle = GameObject.Find("ui/start");
+      goTitle.SetActive(false);
+
+      float buttonHeightStart = Screen.height * 0.20f;
+      float buttonHeightCredits  = Screen.height * 0.15f;
+      float buttonWidthStart = Screen.width * 0.35f;
+      float buttonWidthCredits = Screen.width * 0.25f;
+      rectButtonStart = new Rect(Screen.width * 0.5f - buttonWidthStart * 0.5f, Screen.height * 0.65f, buttonWidthStart, buttonHeightStart);
+      rectButtonCredits = new Rect(Screen.width * 0.5f - buttonWidthCredits * 0.5f, Screen.height * 0.85f, buttonWidthCredits, buttonHeightCredits);
     }
 
     public override void OnEnter(object onEnterParams = null) {
+      goTitle.SetActive(true);
       GameController.Instance.BreakfastSpawner.Cleanup();
       GameController.Instance.MusicManager.PlayTrack(MusicManager.Theme.Intro);
     }
 
     public override void OnLeave() {
-      
+      goTitle.SetActive(false);
     }
 
     public override void OnUpdate() {
       if (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.Return)) {
-        GameController.Instance.ChangeState("GameStatePlaying");
+        StartGame();
+      }
+
+      if (Input.GetMouseButtonUp(0)) {
+        Vector2 mousePosition = Input.mousePosition;
+        mousePosition.y = Screen.height - mousePosition.y;
+
+        if (rectButtonStart.Contains(mousePosition)) {
+          StartGame();
+        }
+
+        if (rectButtonCredits.Contains(mousePosition)) {
+          ShowCredits();
+        }
       }
     }
+
+    //public override void OnGUI() {
+    //  if (GUI.Button(rectButtonStart, "Start")) {
+    //    StartGame();
+    //  }
+    //  if (GUI.Button(rectButtonCredits, "Credits")) {
+    //    ShowCredits();
+    //  }
+    //}
+
+    private void ShowCredits() {
+      GameController.Instance.ChangeState("GameStateCredits");
+    }
+
+    private void StartGame() {
+      GameController.Instance.ChangeState("GameStatePlaying");
+    } 
   }
